8b85e0bca06c3aadd44977dfaebd61cbd025986d
Greg Yabut <greggyabut22@gmail.com>
2025-08-23 20:20:37 +0800

    asdf

---
+++ Greg/PowerShell-Compilation/Absolute-Vs-Relative-Paths.ps1
@@ -0,0 +1,120 @@
+#Absolute and Relative Paths
+
+<#
+Contents:
+
+1. Description
+2. Example 1: User Default Path
+3. Example 2: Changing Directories
+4. Example 3: Changing Directories with Special Characters in Path; Absolute and Relative Path
+5. Example 4: Using Set-Location for a PowerShell Script
+
+#>
+
+
+#Description
+
+<#
+
+Set-Location is used to change directory or folder.
+
+#>
+#end
+
+
+#Example 1: User Default Path
+
+# User Default Path
+
+Set-Location
+
+<#
+
+Wherever path you're in, by issuing the command above, it'll switch you the user default path.
+
+#>
+#End
+
+
+#Example 2: Changing Directories
+
+# To change directory
+
+Set-Location C:\Users\Greg\Desktop\
+
+
+# To change to a specific directory within a directory
+
+Set-Location .\Documents\
+
+#
+
+
+
+
+# Moves up one Directory
+
+Set-Location ..
+
+
+
+
+
+
+# Alias of Set-Location
+
+Get-Alias cd
+
+# NOTE: Alias cd -> Set-Location
+#end
+
+
+#Example 3: Changing Directories with Special Characters in Path; Absolute and Relative Path
+
+# Stays in the current directory
+
+Set-Location .
+
+# Moves up one directory
+
+Set-Location ..
+
+# Goes up two directories
+
+Set-Location ..\..
+
+
+
+<#
+
+Special Characters in Path
+
+.  - Current Path
+.. - Parent Directory
+
+
+
+Absolute Vs. Relative Paths
+
+An absolute path specifies the complete location of a directory, starting from the root of the file system.
+
+        cd C:\Users\Username\Documents
+
+In contrast, a relative path refers to a directory's location in relation to the current directory.
+
+    For instance:
+
+        ..documents/report.pdf
+#>
+#end
+
+
+#Example 4: Using Set-Location for a PowerShell Script
+
+param(
+    [string]$Path
+)
+Set-Location $Path
+
+#To Execute: .\<file.ps1> -Path "C:\MyFiles"
+#end
--- Greg/PowerShell-Compilation/Get-Process.ps1
+++ Greg/PowerShell-Compilation/Get-Process.ps1
@@ -8,7 +8,9 @@ CONTENTS:
 2. Description
 3. EXAMPLES
 3.1. Example 1: Get a list of all running processes on the computer
-
+3.2. Example 2: Display detailed information about one or more processes
+3.3. Example 3: Get all processes with a working set greater than a specified size
+3.4. Example 4: Display processes on the computer in groups based on priority
 
 #>
 #End
@@ -22,6 +24,7 @@ https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.managem
 
 #>
 #end
+
 
 #Description
 
@@ -68,6 +71,7 @@ The actual values when accessed with the member-access operator (.) are in bytes
 #>
 #end
 
+
 #Example 2: Display detailed information about one or more processes
 
 # One Process
@@ -100,4 +104,73 @@ which displays all available properties (*) and their values for each object.
 Get-Process -ID 664
 
 Get-Process -ID 664, 2060
+#end
+
+
+#Example 3: Get all processes with a working set greater than a specified size
+
+# Where-Object
+
+Get-Process | Where-Object { $_.WorkingSet -gt 20917520 }
+
+Get-Process | Where-Object WorkingSet -gt 20MB
+
+
+<#
+The Get-Process cmdlet returns the running processes.
+
+In the context of PowerShell and Windows,
+the "working set" of a process refers to the set of memory pages
+that are currently resident in physical RAM and are actively being used by that process.
+
+
+
+The Get-Process cmdlet returns the running processes.
+
+The output is piped to the Where-Object cmdlet,
+which selects the objects with a WorkingSet value greater than 20,971,520 bytes.
+
+In the first example,
+Where-Object uses a scriptblock to compare the WorkingSet property of each Process object.
+
+
+In the second example,
+the Where-Object cmdlet uses the simplified syntax to compare the WorkingSet property.
+
+In this case, -GT is a parameter, not a comparison operator.
+
+The second example also uses a numeric literal suffix as a concise alternative to 20971520.
+
+In PowerShell, MB represents a mebibyte (MiB) multiplier.
+
+20MB is equal to 20,971,520 bytes.
+
+
+#>
+#end
+
+
+#Example 4: Display processes on the computer in groups based on priority
+
+$processes = Get-Process
+$processes | Sort-Object { $_.PriorityClass } | Format-Table -View Priority
+
+Get-Process | Sort-Object { $_.PriorityClass } | Format-Table -View Priority
+
+<#
+
+These commands display processes on the computer in groups based on their priority class.
+
+The first command gets all processes on the computer and stores them in the $processes variable.
+
+The second command pipes the Process objects stored in the $processes variable to the Sort-Object cmdlet,
+then to the Format-Table cmdlet,
+which formats the processes using the Priority view.
+
+
+In essence:
+Priority Class: sets the overall "importance" of a process.
+Priority (Thread Priority): determines the relative importance of individual tasks within that process.
+
+#>
 #end
