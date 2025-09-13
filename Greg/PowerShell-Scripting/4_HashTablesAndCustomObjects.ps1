66d897ac6e28ad9a2980feff6181afe1afae4330
Greg Yabut <greggyabut22@gmail.com>
2025-09-13 13:30:11 +0800

    asdf

--- Greg/PowerShell-Scripting/4_HashTablesAndCustomObjects.ps1
+++ Greg/PowerShell-Scripting/4_HashTablesAndCustomObjects.ps1
@@ -317,19 +317,231 @@ $myHashTable
 #end
 
 
+<#
+Hashtables can be implmented in inventory sytems, employee database.
+
+e.g. keys: product numbers; barcode numbers
+    values: price
+#>
+
+
+########################################################################
 
 
 
+
+
+# Custom Objects
+
+<#
+
+(1)
+Jacked Programmer Explanation:
+
+Objects are the prvious things we tackled:
+
+    1. Strings
+    2. Intergers
+    3. Hashtables
+
+And we could create our own custom objects.
+
+
+(2)
+
+Everything in PowerShell is an object.
+
+An object is a combination of variables and functions.
+
+Each object has:
+
+    Properties: Variables that describe the object (characteristics).
+    Methods: Functions that define how to use the object (actions).
+
+
+(3)
+
+
+Here are examples of common objects in PowerShell:
+
+1. System Objects
+
+    1.1. Process Objects
+    Get-Process -Name notepad | Select-Object Name, Id, WorkingSet
+
+    1.2 Service Objects
+    Get-Service -Name "BITS" | Select-Object Name, Status, DisplayName
+
+    1.3 File and Folder Objects
+    Get-Item C:\Windows\System32\notepad.exe | Select-Object Name, Length, LastWriteTime
+
+    1.4 String Objects
+    $myString = "Hello, PowerShell!"
+    $myString.Length
+    $myString.ToUpper()
+
+2. Custom Objects
+
+    2.1 PSCustomObject
+
+    $myCustomObject = [PSCustomObject]@{
+        Name = "John Doe"
+        Age = 30
+        City = "New York"
+    }
+    $myCustomObject.Name
+
+
+3. Pipeline Objects
+
+    Objects Pass Through Pipeline:
+    Get-Process | Where-Object {$_.CPU -gt 100} | Select-Object Name, CPU
+
+#>
+
+# Example 1: Creating Custom Objects 1: Longer Method
+
+$Employee1 = New-Object -TypeName PSCustomObject
+
+$Employee1.GetType()
+
+# This is a PSCustomObject
+#end
+
+
+
+
+
+# Example 2: Add-Member
+
+Set-StrictMode -Version Latest
+Set-StrictMode Off
+
+$Employee1 = New-Object -TypeName PSCustomObject
+
+Add-Member -InputObject $Employee1 -MemberType NoteProperty -Name 'EmployeeID' -Value '1001'
+Add-Member -InputObject $Employee1 -MemberType NoteProperty -Name 'FirstName' -Value 'Cristine'
+Add-Member -InputObject $Employee1 -MemberType NoteProperty -Name 'Title' -Value 'Nurse'
+
+$Employee1.Length
+
+$Employee1.EmployeeID
+$Employee1.FirstName
+$Employee1.Title
+
+$Employee1
+
+#end
+
+
+
+
+
+# Example 3: Get-Member
+
+$Employee1 = New-Object -TypeName PSCustomObject
+
+Add-Member -InputObject $Employee1 -MemberType NoteProperty -Name 'EmployeeID' -Value '1001'
+Add-Member -InputObject $Employee1 -MemberType NoteProperty -Name 'FirstName' -Value 'Cristine'
+Add-Member -InputObject $Employee1 -MemberType NoteProperty -Name 'Title' -Value 'Nurse'
+
+Get-Member -InputObject $Employee1
+$Employee1 | Get-Member # same as the above
+
+$Employee1
+#end
+
+
+
+
+
+
+# Example 4: Creating Custom Objects 2
+
+# Syntax:
+$Employee2 = [PSCustomObject]@{
+    Keys = "Value"
+}
+
+$Employee2 = [PSCustomObject]@{
+    EmployeeID = "1002"
+    FirstName = 'Bernadette'
+    Title = 'Nurse'
+}
+
+$Employee2
+
+
+
+# One Liner
+$Employee3 = [PSCustomObject]@{EmployeeID = "1002"; FirstName = 'Cristiniee'; Title = 'Tiktoker'}
+
+$Employee3
+#end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Summary: ArrayList
+
+$mylist1 = [System.Collections.ArrayList]@()
+
+$mylist1.GetType()
+
+
+$mylist2 = New-Object -TypeName System.Collections.ArrayList
+
+$mylist2.GetType()
+
+$myList2.IsFixedSize
+
+$mylist2.Add("Arraylist")
+[void]$mylist2.Add("Arraylist")
+
+$mylist2.AddRange(@("Sample1", "Sample2"))
+
+
+
+$mylist2.Remove("Sample1")
+$mylist2.RemoveAt(3)
+
+$mylist2.RemoveRange(0,1)
+$mylist2
+
+
+
+
+# Summary: Array
 
 Set-StrictMode -Version Latest
 Set-StrictMode Off
 
 $myArray = @( "A", "B", "C" )
 
+$myArray.GetType()
+$myArray.IsFixedSize
+$myArray.Count
+$myArray.Length
+
+$myArray[2]
+
 $myArray = $myArray + "D"
+
+$myArray[3]
 
 $myArray += "DE"
 
+$myArray.RemoveAt(1) # not gonnna work; array is fixed.
 $myArray = $myArray -ne "A"
 
 $myArray
